d689ffac2c87f0fead611202c4d720c999105d72
Glowstik_ <69128866+Glowstik-YT@users.noreply.github.com>
2022-04-29 17:56:46 -0400

    reverting changes

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ import nextcord
 from nextcord.ext import commands, tasks
 from nextcord.abc import GuildChannel
 from nextcord import Interaction, ChannelType
-from config import CONFIGTOKEN, PREFIX, OWNER
+from config import TOKEN, PREFIX, OWNER
 import os
 import aiosqlite
 
@@ -80,4 +80,4 @@ async def on_application_command_error(ctx, error):
             await ctx.send(embed=em, delete_after=30)
             return
 
-bot.run(os.getenv(TOKEN)) #replace this with bot.run(CONFIGTOKEN)
+bot.run(TOKEN)
